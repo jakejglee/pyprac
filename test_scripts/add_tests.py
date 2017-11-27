a1c4aa48214d583b4888fe0048efcdfc97e7d06d
jakejglee <jgieferlee@gmail.com>
2017-11-27 13:15:23 -0800

    switching to python scripts...

--- test_scripts/add_tests.py
+++ test_scripts/add_tests.py
@@ -1,16 +1,19 @@
-#!/bin/bash
-# add_tests.sh
+#!/usr/bin/env python
+# add_tests.py
 # DESCRIPTION: This script initializes new files taken from the 'testlist' file containing the names of python practice scripts.
-# USAGE: chmod u+x add_tests.sh; add_tests
+# USAGE: chmod a+x add_tests.sh; add_tests
 
-for i in $( cat testlist.txt ); do
-	if [ ! -e $i ]
-	then 
-		printf "Building $i...\n"
-		if [[ grep "$i" : 'warmup\-2'` == 8 ]]; then
-			printf "\tadding header to $i.py...\n"
-			printf "# Warmup-2: " >> "$i.py"
-		fi
+# Imported
+import os
+import re
+
+r = open('testlist.txt', 'r')
+for test in r:
+    if not os.path.isfile(test):
+		print "Building" + test "...\n"
+                if re.match('.*warmup-1.*', test):
+			print "\tadding header to $i.py...\n"
+			print "# Warmup-2: " >> "$i.py"
 		else if [[ "$i" =~ ^(.*string\-1.*) ]]; then
 			printf "\tadding header to $i.py...\n"
 			printf "# String-1: " >> "$i.py"
